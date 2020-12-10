2e734e5d6a014da45ff60744d457e14e89270ea9
Vishal Balaji Sivaraman <64977199+The-SocialLion@users.noreply.github.com>
2020-12-10 17:26:47 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,35 @@
+from keras.models import model_from_json # used to import model
+import numpy as np
+from keras.preprocessing import image# used for preproccesing 
+# loading or reading the saved model ,weights
+json_file=open('model.json','r')
+loaded_model_json=json_file.read()
+json_file.close()
+model=model_from_json(loaded_model_json)
+model.load_weights("model.h5")
+print("loaded model from disk")
+#classification of images
+def classify(img_file):
+    img_name=img_file
+    test_image=image.load_img(img_name,target_size=(64,64))
+    test_image=image.img_to_array(test_image)
+    test_image=np.expand_dims(test_image,axis=0)
+    result=model.predict(test_image)
+
+    if result[0][0]==1:
+        prediction='Recyclable(R)'
+    else:
+        prediction='Organic(O)'
+    print(prediction,img_name)
+#storing the images in this folder
+import os
+path='D:\python\dl programs\Waste Management\DATASET\TEST'
+files=[]
+# r=root,d=directories,f=files
+for r,d,f in os.walk(path):
+    for file in f:
+        if '.jpeg' or '.jpg' in file:
+            files.append(os.path.join(r,file))
+for f in files:
+    classify(f)
+    print('\n')
